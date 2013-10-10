4c3703679da1662b31c0214b98c718ad1d6de6de
Hackbright Student <user@mini-005.(none)>
2013-10-09 17:47:23 -0700

    Worked through SQL exercises

--- hackbright_app.py
+++ hackbright_app.py
@@ -16,6 +16,20 @@ def connect_to_db():
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+def get_project_by_title(title):
+    query = """SELECT title FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Project: %s   """ %(row[0])
+
 def main():
     connect_to_db()
     command = None
@@ -29,6 +43,8 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "project_search":
+            get_project_by_title(*args)
 
     CONN.close()
 
